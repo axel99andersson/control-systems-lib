71c09a6e4f03e0fe1530cb2533ea0e561e503ca8
Axel Andersson <axel.g.e.andersson@gmail.com>
2025-01-09 16:09:48 +0100

    added own class for keeping metrics

---
+++ metrics.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class MetricsTracker:
+    def __init__(self):
+        self.states = []
+        self.measurements = []
+        self.state_estimates = []
+        self.control_signals = []
+        self.residuals = []
+
+    def track(self, state, estimated_state, measurement, control_signal, residuals):
+        self.states.append(state)
+        self.state_estimates.append(estimated_state)
+        self.measurements.append(measurement)
+        self.control_signals.append(control_signal)
+        self.residuals.append(residuals)
+
+    def get_metrics(self):
+        return [self.states, 
+                self.state_estimates, 
+                self.measurements, 
+                self.control_signals, 
+                self.residuals]
+    
+    def residual_statistics(self):
+        abs_residuals = np.absolute(self.residuals)
+        mean, std = abs_residuals.mean(), abs_residuals.std()
+        return mean, std
+
+def plot_figs(time, tracker: MetricsTracker):
+    states, state_estimates, measurements, control_signals, residuals = tracker.get_metrics()
+
+    states = np.array(states)
+    state_estimates = np.array(state_estimates)
+    measurements = np.array(measurements)
+    throttles = np.array(control_signals)
+
+    # Plot position and velocity
+    plt.figure(figsize=(12, 8))
+    plt.subplot(2, 1, 1)
+    plt.plot(time, states[:, 0], label="Position (m)")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Position (m)")
+    plt.title("Vehicle Position")
+    plt.legend()
+
+    plt.subplot(2, 1, 2)
+    plt.plot(time, states[:, 1], label="True Velocity (m/s)", color='orange')
+    plt.plot(time, measurements, label="Measurement", color='red')
+    plt.plot(time, state_estimates[:,1], label="Estimated Velocity", color='blue')
+    plt.vlines(10, 0, 30)
+    plt.vlines(20, 0, 30)
+    plt.xlabel("Time (s)")
+    plt.ylabel("Velocity (m/s)")
+    plt.title("Vehicle Velocity")
+    plt.legend()
+
+    plt.figure(figsize=(6, 4))
+    plt.plot(time, throttles, label="Throttle")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Throttle")
+    plt.legend()
+
+    plt.figure(figsize=(6, 4))
+    plt.plot(time, np.absolute(residuals), label="Absolute Residuals")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Residual")
+    plt.legend()
+
+    plt.show()
